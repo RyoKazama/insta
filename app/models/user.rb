13d4682c6cc864256f8c4cb28a488eb9f1e0824c
風間亮 <ryokazama@kazamaryou-no-MacBook-Air.local>
2017-06-20 22:27:46 +0900

    エラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
 
-  validates :name, presence: true
   mount_uploader :avatar, AvatarUploader
 
   has_many :pictures
